a02daf91437206ffa52b0255827d0c41e828babc
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-12-04 11:35:46 +0530

    Add implementation for isAnagram function

--- week-01/easy/anagram.js
+++ week-01/easy/anagram.js
@@ -4,6 +4,14 @@
   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
 */
 
-function isAnagram(str1, str2) {}
+function isAnagram(str1, str2) {
+  let sortedStr1 = str1.toLowerCase().split('').sort().join('');
+  let sortedStr2 = str2.toLowerCase().split('').sort().join('');
+  if (sortedStr1 === sortedStr2) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 module.exports = isAnagram;
