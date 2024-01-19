146a917f445d7320b8a43e6ae24c35205c6c4df7
Haseeb Yousuf <haseebyousuff@gmail.com>
2024-01-19 21:38:06 +0530

    Add RecoilRoot and import GithubProfile component

--- week-07/src/App.jsx
+++ week-07/src/App.jsx
@@ -1,17 +1,21 @@
+import { RecoilRoot } from 'recoil';
 import ProfileCard from './assignments/Assignment1/ProfileCard';
 import BackgroundChanger from './assignments/assignment2/BackgroundChanger';
 import ParaGenerator from './assignments/assignment4/ParaGenerator';
+import GithubProfile from './assignments/assignment5/GithubProfile';
 
 function App() {
   return (
-    <>
+    <RecoilRoot>
       {/*  Assignment 1 */}
       {/* <ProfileCard /> */}
       {/*  Assignment 2 */}
       {/* <BackgroundChanger /> */}
       {/*  Assignment 4 */}
-      <ParaGenerator />
-    </>
+      {/* <ParaGenerator /> */}
+      {/*  Assignment 5 */}
+      <GithubProfile />
+    </RecoilRoot>
   );
 }
 
