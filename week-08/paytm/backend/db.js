a5461e92867b2c076fb18f6e8a099d7f1c2eff24
Haseeb Yousuf <haseebyousuff@gmail.com>
2024-01-24 23:39:32 +0530

    Add User model to backend

---
+++ week-08/paytm/backend/db.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const UserSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    minLength: 3,
+    maxLength: 30,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+});
+
+export const User = mongoose.model('User', UserSchema);
