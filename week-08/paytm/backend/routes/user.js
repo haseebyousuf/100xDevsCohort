b6a82a8b1dce6d33cd1241618f9217ef00e9f2ae
Haseeb Yousuf <haseebyousuff@gmail.com>
2024-01-25 02:02:36 +0530

    Add updateUser route and authMiddleware

--- week-08/paytm/backend/routes/user.js
+++ week-08/paytm/backend/routes/user.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { signin, signup } from '../controllers/user.controllers.js';
+import { signin, signup, updateUser } from '../controllers/user.controllers.js';
+import { authMiddleware } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
 router.post('/signup', signup);
 
 router.post('/signin', signin);
+
+router.put('/', authMiddleware, updateUser);
 
 export default router;
