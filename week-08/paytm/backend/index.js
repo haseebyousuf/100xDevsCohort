74ccf8817e354a8c04aa10bddad16a56f92e6bc1
Haseeb Yousuf <haseebyousuff@gmail.com>
2024-01-25 01:09:02 +0530

    Update index.js with ES6 imports and add rootRouter

--- week-08/paytm/backend/index.js
+++ week-08/paytm/backend/index.js
@@ -1,3 +1,31 @@
-const express = require("express");
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
+import rootRouter from './routes/index.js';
 
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const startServer = async () => {
+  try {
+    mongoose.connect(`${process.env.MONGO_URI}`).then(() => {
+      console.log('db connected');
+      app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+      });
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
+
+app.use('/api/v1', rootRouter);
