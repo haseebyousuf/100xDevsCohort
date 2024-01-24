832674623adb00451887df3e65f25f84d49678b2
Haseeb Yousuf <haseebyousuff@gmail.com>
2024-01-25 01:19:40 +0530

    Update email validation to include maximum length

--- week-08/paytm/backend/utils/zodSchemas.js
+++ week-08/paytm/backend/utils/zodSchemas.js
@@ -1,7 +1,7 @@
 import zod from 'zod';
 
 export const signUpSchema = zod.object({
-  username: zod.string().email(),
+  username: zod.string().email().max(30),
   firstName: zod.string(),
   lastName: zod.string(),
   password: zod.string(),
