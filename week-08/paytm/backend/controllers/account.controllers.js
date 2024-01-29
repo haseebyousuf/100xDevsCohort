a181f7569c2f6615fa0894e0569e4796819baec1
Haseeb Yousuf <haseebyousuff@gmail.com>
2024-01-29 18:55:39 +0530

    Add account controllers for balance and transfer

---
+++ week-08/paytm/backend/controllers/account.controllers.js
@@ -0,0 +1,65 @@
+import mongoose from 'mongoose';
+import { Account } from '../db';
+
+export const balance = async (req, res) => {
+  try {
+    const account = await Account.findOne({
+      userId: req.userId,
+    });
+
+    res.json({
+      balance: account.balance,
+    });
+  } catch (error) {
+    return res.status(401).json({ message: 'Server Error' });
+  }
+};
+
+export const transfer = async (req, res) => {
+  try {
+    const session = await mongoose.startSession();
+
+    session.startTransaction();
+    const { amount, to } = req.body;
+
+    // Fetch the accounts within the transaction
+    const account = await Account.findOne({ userId: req.userId }).session(
+      session
+    );
+
+    if (!account || account.balance < amount) {
+      await session.abortTransaction();
+      return res.status(400).json({
+        message: 'Insufficient balance',
+      });
+    }
+
+    const toAccount = await Account.findOne({ userId: to }).session(session);
+
+    if (!toAccount) {
+      await session.abortTransaction();
+      return res.status(400).json({
+        message: 'Invalid account',
+      });
+    }
+
+    // Perform the transfer
+    await Account.updateOne(
+      { userId: req.userId },
+      { $inc: { balance: -amount } }
+    ).session(session);
+    await Account.updateOne(
+      { userId: to },
+      { $inc: { balance: amount } }
+    ).session(session);
+
+    // Commit the transaction
+    await session.commitTransaction();
+
+    res.json({
+      message: 'Transfer successful',
+    });
+  } catch (error) {
+    return res.status(401).json({ message: 'Server Error' });
+  }
+};
