780907acbb52836ada4c39c90bf5ff694f032ae0
Haseeb Yousuf <haseebyousuff@gmail.com>
2024-01-30 16:48:24 +0530

    Add Recoil and Toast components to main.jsx

--- week-08/paytm/frontend/src/main.jsx
+++ week-08/paytm/frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { RecoilRoot } from 'recoil';
+import { Toaster } from 'react-hot-toast';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RecoilRoot>
+      <App />
+      <Toaster />
+    </RecoilRoot>
+  </React.StrictMode>
+);
