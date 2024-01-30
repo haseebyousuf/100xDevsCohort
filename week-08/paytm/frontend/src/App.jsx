1e8d48739cb2c2ba5c1b6a57458a8b5954600abb
Haseeb Yousuf <haseebyousuff@gmail.com>
2024-01-30 16:48:15 +0530

    Refactor routing in App component

--- week-08/paytm/frontend/src/App.jsx
+++ week-08/paytm/frontend/src/App.jsx
@@ -1,11 +1,38 @@
-
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Signup from './pages/Signup';
+import Dashboard from './pages/Dashboard';
+import SendMoney from './pages/SendMoney';
+import Signin from './pages/Signin';
+import { useRecoilValue } from 'recoil';
+import { userAtom } from './atoms/userAtom';
 function App() {
-
+  const user = useRecoilValue(userAtom);
   return (
-    <div>
-        Hello world
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path='/'
+          element={!user ? <Signin /> : <Navigate to='/dashboard' />}
+        />
+        <Route
+          path='/signup'
+          element={!user ? <Signup /> : <Navigate to='/dashboard' />}
+        />
+        <Route
+          path='/signin'
+          element={!user ? <Signin /> : <Navigate to='/dashboard' />}
+        />
+        <Route
+          path='/dashboard'
+          element={user ? <Dashboard /> : <Navigate to='/signin' />}
+        />
+        <Route
+          path='/send'
+          element={user ? <SendMoney /> : <Navigate to='/signin' />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
