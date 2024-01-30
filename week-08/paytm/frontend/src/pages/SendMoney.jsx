51b1f43f3657c8c6b725e5d48451bd7e20b17b48
Haseeb Yousuf <haseebyousuff@gmail.com>
2024-01-30 16:49:41 +0530

    Add SendMoney component for money transfer

---
+++ week-08/paytm/frontend/src/pages/SendMoney.jsx
@@ -0,0 +1,84 @@
+import axios from 'axios';
+import { useState } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { toast } from 'react-hot-toast';
+import { useRecoilState } from 'recoil';
+import { userAtom } from '../atoms/userAtom';
+
+const SendMoney = () => {
+  const [searchParams] = useSearchParams();
+  const id = searchParams.get('id');
+  const name = searchParams.get('name');
+  const [amount, setAmount] = useState(0);
+  const [user, setUser] = useRecoilState(userAtom);
+  const navigate = useNavigate();
+
+  const handleTransfer = async () => {
+    try {
+      const res = await axios.post(
+        'http://localhost:5001/api/v1/account/transfer',
+        { to: id, amount },
+        {
+          headers: {
+            Authorization: 'Bearer ' + localStorage.getItem('token'),
+          },
+        }
+      );
+      toast.success(res.data.message);
+      setUser({ ...user, balance: res.data.balance });
+      localStorage.setItem('user', JSON.stringify(user));
+
+      navigate('/dashboard');
+    } catch (error) {
+      toast.error(error?.response?.data?.message || 'There is some error');
+    }
+  };
+  return (
+    <div className='flex justify-center h-screen bg-gray-100'>
+      <div className='flex flex-col justify-center h-full'>
+        <div className='max-w-md p-4 space-y-8 bg-white border rounded-lg shadow-lg h-min text-card-foreground w-96'>
+          <div className='flex flex-col space-y-1.5 p-6'>
+            <h2 className='text-3xl font-bold text-center'>Send Money</h2>
+          </div>
+          <div className='p-6'>
+            <div className='flex items-center space-x-4'>
+              <div className='flex items-center justify-center w-12 h-12 bg-green-500 rounded-full'>
+                <span className='text-2xl text-white'>
+                  {name[0].toUpperCase()}
+                </span>
+              </div>
+              <h3 className='text-2xl font-semibold'>{name}</h3>
+            </div>
+            <div className='space-y-4'>
+              <div className='space-y-2'>
+                <label
+                  className='text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
+                  htmlFor='amount'
+                >
+                  Amount (in Rs)
+                </label>
+                <input
+                  onChange={(e) => {
+                    setAmount(e.target.value);
+                  }}
+                  type='number'
+                  className='flex w-full h-10 px-3 py-2 text-sm border rounded-md border-input bg-background'
+                  id='amount'
+                  placeholder='Enter amount'
+                />
+              </div>
+              <button
+                onClick={handleTransfer}
+                className='justify-center w-full h-10 px-4 py-2 text-sm font-medium text-white transition-colors bg-green-500 rounded-md ring-offset-background'
+              >
+                Initiate Transfer
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SendMoney;
